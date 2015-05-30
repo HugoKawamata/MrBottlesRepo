38c9d30154916831c38de2b75f961afccfe1bcc3
wolftongue <hugokawamata@gmail.com>
2015-05-30 23:28:48 +1000

    Lots of changes to html and css

--- files/js/site.js
+++ files/js/site.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	
+	$(".gallery_expand").hide(); //hide expanded navigation sections on page load
+		 var time;
+		 function hideExpandedNav() {
+			 $(".gallery_expand").fadeOut(50);
+		 };
+		 $(".gallery").mouseover(function () {
+			 $(".gallery_expand").show(); //expanded navigation section is revealed when the relevant text is moused over
+			 clearTimeout(time); //stop the expanded section from hiding if the timer is counting down
+		 });
+		 $(".gallery").mouseleave(function () {
+			 time = setTimeout(hideMe, 200); //prevents the expanded section from hiding if the user accidentally mouses out quickly
+		 });
 })
