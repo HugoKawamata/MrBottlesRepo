32ad4a13a78c5aa110da4df29b50e7c796b1e2eb
wolftongue <hugokawamata@gmail.com>
2015-06-01 16:32:28 +1000

    Expanded Navigation js and html complete

--- files/js/site.js
+++ files/js/site.js
@@ -1,14 +1,45 @@
 $(document).ready(function(){
-	$(".gallery_expand").hide(); //hide expanded navigation sections on page load
-		 var time;
-		 function hideExpandedNav() {
-			 $(".gallery_expand").fadeOut(50);
+	console.log("meme")
+	$("#services_expanded").hide()
+	$("#community_expanded").hide()
+	$("#gallery_expanded").hide(); //hide expanded navigation sections on page load
+	
+//The following are 3 functions which hide and show different navigation sections
+	var countdownServices;
+	function hideExpandedServices() {
+			 $("#services_expanded").fadeOut(50);
 		 };
-		 $(".gallery").mouseover(function () {
-			 $(".gallery_expand").show(); //expanded navigation section is revealed when the relevant text is moused over
-			 clearTimeout(time); //stop the expanded section from hiding if the timer is counting down
+		 $("#services").mouseover(function () {
+			 $("#services_expanded").show(); //expanded navigation section is revealed when the relevant text is moused over
+			 clearTimeout(countdownServices); //stop the expanded section from hiding if the timer is counting down
 		 });
-		 $(".gallery").mouseleave(function () {
-			 time = setTimeout(hideMe, 200); //prevents the expanded section from hiding if the user accidentally mouses out quickly
+		 $("#services").mouseleave(function () {
+			 countdownServices = setTimeout(hideExpandedServices, 100); //prevents the expanded section from hiding if the user accidentally mouses out quickly
 		 });
+		 
+	var countdownCommunity;	 
+	function hideExpandedCommunity() {
+			 $("#community_expanded").fadeOut(50);
+		 };
+		 $("#community").mouseover(function () {
+			 $("#community_expanded").show(); //expanded navigation section is revealed when the relevant text is moused over
+			 clearTimeout(countdownCommunity); //stop the expanded section from hiding if the timer is counting down
+		 });
+		 $("#community").mouseleave(function () {
+			 countdown = setTimeout(hideExpandedCommunity, 200); //prevents the expanded section from hiding if the user accidentally mouses out quickly
+		 });	 
+		 
+	var countdownGallery;
+	function hideExpandedGallery() {
+			 $("#gallery_expanded").fadeOut(50);
+		 };
+		 $("#gallery").mouseover(function () {
+			 $("#gallery_expanded").show(); //expanded navigation section is revealed when the relevant text is moused over
+			 clearTimeout(countdownGallery); //stop the expanded section from hiding if the timer is counting down
+		 });
+		 $("#gallery").mouseleave(function () {
+			 countdownGallery = setTimeout(hideExpandedGallery, 100); //prevents the expanded section from hiding if the user accidentally mouses out quickly
+		 });
+		 
+
 })
